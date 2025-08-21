dc1be3133c8ac1aa3e3029ca22d716a18271c731
jto05 <julian.to2023@gmail.com>
2025-08-21 02:45:09 -0700

    Added basic server request handling
    - Server will obtain basic string messages from client console and
    return it to the server console

--- server.go
+++ server.go
@@ -1,46 +1,71 @@
 package server
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net"
 )
 
 type Server struct {
-	address string
+	host string
+	port string
 }
 
-func New(address string) *Server {
+type Client struct {
+	conn net.Conn
+}
+
+type Config struct {
+	Host string
+	Port string
+}
+
+func New(config *Config) *Server {
 	return &Server{
-		address,
+		host: config.Host,
+		port: config.Port,
 	}
 }
 
 /*
 Start contains
 */
-func (s *Server) Start() {
-	listener, err := net.Listen("tcp", s.address)
+func (s *Server) Run() {
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%s", s.host, s.port))
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
 	defer listener.Close()
 
-	fmt.Println("Listening on " + s.address + "...")
+	fmt.Printf("Listening on %s:%s...\n", s.host, s.port)
 
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Fatal(err)
 		}
-		go s.handleConnections(conn)
 
+		client := &Client{
+			conn: conn,
+		}
+
+		go client.handleRequest()
 	}
 }
 
-func (s *Server) handleConnections(conn net.Conn) {
-	defer conn.Close()
+func (client *Client) handleRequest() {
+	reader := bufio.NewReader(client.conn)
+	for { // loops until connection on Client's until error received or connection closed
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			client.conn.Close()
+			return
+		}
 
-	fmt.Printf("Receiving: %f")
+		fmt.Printf("Message incoming: %s", string(message))
+		client.conn.Write([]byte("Message received. \n"))
+	}
 }
