3127e09db877808dbd08a4b2899a09d2ca0ea386
jto05 <julian.to2023@gmail.com>
2025-08-25 19:35:04 -0700

    Added basic user input for client package

--- client/client.go
+++ client/client.go
@@ -1,27 +1,53 @@
 package client
 
 import (
+	"bufio"
 	"fmt"
-	"log"
-	"net"
+	"io"
+	// "log"
+	// "net"
 )
 
 type Client struct {
-	host string
-	port string
+	host   string
+	port   string
+	reader io.Reader
+	writer io.Writer
 }
 
 type Config struct {
-	Host string
-	Port string
+	Host   string
+	Port   string
+	Reader io.Reader
+	Writer io.Writer
 }
 
 func New(config *Config) (client *Client) {
 	return &Client{
-		host: config.Host,
-		port: config.Port,
+		host:   config.Host,
+		port:   config.Port,
+		reader: config.Reader,
+		writer: config.Writer,
 	}
 }
 
-func (client *Client) Run() {
+// TODO: allow client to send as many requests to the server as they want
+func (c *Client) Run() {
+	sc := bufio.NewScanner(c.reader)
+	// conn, err := net.Dial("tcp", fmt.Sprintf("%s:%s", c.host, c.port))
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// a scanner?
+	fmt.Fprintln(c.writer, "Enter text:")
+	for sc.Scan() {
+		line := sc.Text()
+		fmt.Fprintln(c.writer, "You wrote: "+line)
+
+		if line == "exit" {
+			fmt.Fprintln(c.writer, "goodbye!")
+			break
+		}
+		// conn.Write([]byte(input))
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module terminal_chatroom
 
 go 1.23.4
+
+require github.com/google/go-cmp v0.7.0
