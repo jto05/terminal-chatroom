bd0754de2f0fbaa7b6d851835f702f2d95cc050b
jto05 <julian.to2023@gmail.com>
2025-08-25 19:34:47 -0700

    Added test for basic user input for client packagae

--- client/client_test.go
+++ client/client_test.go
@@ -1,10 +1,33 @@
 package client_test
 
 import (
+	"bytes"
+	"strings"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 
 	"terminal_chatroom/client"
 )
 
 func TestRun(t *testing.T) {
+	input := strings.NewReader("hello world!\nexit\n")
+	output := &bytes.Buffer{}
+
+	want := "Enter text:\nYou wrote: hello world!\nYou wrote: exit\ngoodbye!\n"
+
+	c := client.New(&client.Config{
+		Host:   "localhost",
+		Port:   "3333",
+		Reader: input,
+		Writer: output,
+	})
+
+	c.Run()
+
+	got := output.String()
+
+	if want != got {
+		t.Error(cmp.Diff(want, got))
+	}
 }
