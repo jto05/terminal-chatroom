9cdfc857f7e259c3011c86a1824e8816ff06e8f1
jto05 <julian.to2023@gmail.com>
2025-09-20 10:43:54 -0700

    Updated user interface for client

--- client/client.go
+++ client/client.go
@@ -46,33 +46,36 @@ func (c *Client) Run() {
 
 	sc := bufio.NewScanner(c.input)
 
-	fmt.Fprintln(c.output, "Hello!")
+	fmt.Fprintf(c.output, "Hello! You're dialing %s:%s\n", c.host, c.port)
+	fmt.Fprintln(c.output, "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
 	// change username if wasn't initialized in config
 	if c.username == "" {
+		fmt.Fprintln(c.output, "Enter username:")
 		if sc.Scan() {
 			c.username = strings.TrimSpace(sc.Text())
 		}
+		fmt.Fprintln(c.output, "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 	}
 
 	go c.receiveServerData(conn) // start getting data from server
 
 	for {
 		fmt.Fprintf(c.output, "%s : ", c.username)
+
 		if !sc.Scan() {
 			break
 		}
-		line := sc.Text()
 
-		line = strings.TrimSpace(line)
+		line := strings.TrimSpace(sc.Text())
 
 		if line == "/exit" {
-			fmt.Fprintln(c.output, "goodbye!")
+			fmt.Fprintln(c.output, "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+			fmt.Fprintln(c.output, "Goodbye!")
 			break
 		}
 
 		msg := c.username + " : " + line
-		c.last_printed_line = msg
 
 		_, err := conn.Write([]byte(msg + "\n"))
 		if err != nil {
@@ -90,20 +93,16 @@ func (c *Client) receiveServerData(conn net.Conn) {
 		}
 		msg = strings.TrimSpace(msg)
 
-		split_msg := strings.Split(msg, " : ")
+		split_msg := strings.Split(msg, " : ") // split message into username and content
 
 		if msg == "" {
 			continue
 		}
 
 		if split_msg[0] != c.username { // only print if the message written was NOT made from user
-
-			// clear current input line, print message, then reprint prompt
 			fmt.Fprint(c.output, "\r\033[K")           // clear line
-			fmt.Fprintln(c.output, msg)                // the incoming message
-			fmt.Fprintf(c.output, "%s : ", c.username) // prompt
-			c.last_printed_line = c.username + " : "
-
+			fmt.Fprintln(c.output, msg)                // print the incoming message
+			fmt.Fprintf(c.output, "%s : ", c.username) // print the prompt
 		}
 	}
 }
